86b3f9896a5523f861e0bc0d0a03bf05b6b00a62
Airis1k <airingasstar@gmail.com>
2022-03-14 17:27:40 +0200

    pakoreguota prisijungimo forma

--- src/main/java/com/example/filmai/MainApplication.java
+++ src/main/java/com/example/filmai/MainApplication.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Filmai");
+        // scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setTitle("Prisijungimo langas");
         stage.setScene(scene);
         stage.show();
     }
